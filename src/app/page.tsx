37f5fff5add8d1239e446fe080041b367bb2c082
cyanxcode <151661246+cyanxcode@users.noreply.github.com>
2024-05-27 22:10:14 +0530

    Hero Section added and about us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
-export default function Home() {
+import Image from "next/image";
+
+export default function Option() {
   return (
     <main>
-      <h2 className="text-2xl font-bold">Hello World!</h2>
+      <div className="relative ml-[50%] w-[95%] translate-x-[-50%] aspect-video overflow-hidden max-h-[80vh]">
+        <img
+          src="/bg1.jpg"
+          className="absolute object-cover w-full h-full rounded-2xl object-left-top"
+          alt="bg"
+        />
+        <h3 className="text-xl sm:text-2xl md:text-3xl lg:text-5xl font-bold text-white w-[40%] absolute m-5 sm:m-10 md:m-15 lg:m-20">
+          A black belt is just a white belt who never quits.
+        </h3>
+      </div>
+      <section className="mt-16 md:mt-32 w-full">
+        <h2 className="text-2xl sm:text-3xl md:text-5xl font-extrabold text-center">
+          About Us
+        </h2>
+        <p className="text-sm sm:text-md md:text-xl text-center my-10 w-[80%] max-w-[1000px] ml-[50%] translate-x-[-50%]">
+          We provide training in <b>Karate and Mix Martial Arts</b> teaching
+          both beginners and experts. Karate emphasizes discipline and respect
+          alongside physical techniques. Training includes developing focus,
+          breath control and mental strength.
+        </p>
+      </section>
     </main>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { RiMenu2Fill } from "react-icons/ri";
+export default function Header() {
+  return (
+    <nav className="w-full h-20 flex justify-between items-center px-10">
+      <div className="text-xl font-extrabold flex">
+        {" "}
+        <RiMenu2Fill size={25} className="mt-1 mr-16" />
+        <Image
+          src="/UFC.jpg"
+          width={30}
+          height={5}
+          alt="UFC"
+          className="mr-4"
+        />
+        Unbeatable Fight Club
+      </div>
+      <div className="flex gap-6 hidden md:visible">
+        <li className=" text-md font-medium list-none">Pricing</li>
+        <li className=" text-md font-medium list-none">About</li>
+        <li className=" text-md font-medium list-none">Contact</li>
+      </div>
+    </nav>
+  );
+}
